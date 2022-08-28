1f4ce8c183a224bdeaffae832c787b8f5106c1b7
Daniel Coimbra <danielcrmnzs@hotmail.com>
2022-08-28 10:39:38 -0300

    Inserção de usuário com POST

--- src/main/java/com/dcoimbra/workshopmongo/services/UserService.java
+++ src/main/java/com/dcoimbra/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.dcoimbra.workshopmongo.domain.User;
+import com.dcoimbra.workshopmongo.dto.UserDTO;
 import com.dcoimbra.workshopmongo.repositories.UserRepository;
 import com.dcoimbra.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -23,6 +24,14 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> optUser = userRepository.findById(id);
 		return optUser.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+	public User insert(User user) {
+		return userRepository.insert(user);
+	}
+
+	public User fromDTO(UserDTO userDTO) {
+		return new User(userDTO.getId(), userDTO.getName(), userDTO.getEmail());
 	}
 
 }
